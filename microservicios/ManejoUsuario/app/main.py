eaa156bded647fe2a97ec3cb838a9e67c2436c54
edysaul20000 <edysaulserrano@gmail.com>
2025-07-07 15:36:21 -0500

    feat: Implementando microservicio main.py de ManejoUsuario

---
+++ microservicios/ManejoUsuario/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+usuarios= {}
+user_id_seq = 1
+
+@app.post("/usuarios")
+def creacion_usuario(nombre: str):
+    global usuario_id_seq
+    usuario = {"id": usuario_id_seq, "nombre": nombre}
+    usuario[usuario_id_seq] = usuario
+    usuario_id_seq += 1
+    return usuario
+
+@app.get("/usuarios")
+def lista_usuarios():
+    return list(usuarios.values())
+
+@app.delete("/usuarios/{usuario_id}")
+def eliminar_usuario(usuario_id: int):
+    if usuario_id in usuarios:
+        del usuarios[usuario_id]
+        return {"mensaje": "Usuario eliminado"}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
